2584650db91662bd8ac8b9d909891f5048ac91cb
barendan <barendan@gmail.com>
2022-01-15 09:32:09 -0500

    create app architecture and add basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink
+} from 'react-router-dom';
+import GetMoney from './containers/GetMoney';
+import TodoApp from './containers/TodoApp';
+import TradeStop from './containers/TradeStop';
+import Calendar from './containers/Calendar';
+import BlogPage from './containers/BlogPage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const Home = () => <>
+    <h1 style={{ padding: "1.2rem", fontSize: "42px"}}> Portal Directory</h1>
+    <ul>
+      <li style={{padding: "1.5rem", fontSize: "24px"}}>  
+        <NavLink to="calendar">
+          Calendar
+        </NavLink> 
+      </li>
+      <li style={{padding: "1.5rem", fontSize: "24px"}}>  
+        <NavLink to="blog">
+          Blog Page
+        </NavLink> 
+      </li>
+      <li style={{padding: "1.5rem", fontSize: "24px"}}>  
+        <NavLink to="trade">
+          TradeStop
+        </NavLink> 
+      </li>
+      <li style={{padding: "1.5rem", fontSize: "24px"}}>  
+        <NavLink to="million">
+          Quick Milly
+        </NavLink> 
+      </li>
+      <li style={{padding: "1.5rem", fontSize: "24px"}}>  
+        <NavLink to="todo">
+          Mini Todo App
+        </NavLink> 
+      </li>
+    </ul>
+</>
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/trade" element={<TradeStop />} />
+          <Route path="/million" element={<GetMoney />} />
+          <Route path="/todo" element={<TodoApp />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
---
+++ src/containers/Calendar.js
@@ -0,0 +1,11 @@
+import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+
+const Calendar = () => {
+  return (
+    <>
+      <h1>Calendar Page</h1>
+    </>
+  )
+}
+
+export default Calendar;
