6d6788d3b82d902e9030d3be83bdcc32deee4afd
barendan <barendan@gmail.com>
2022-01-16 18:16:01 -0500

    enhanced blog post component and built out blog page

--- src/components/blogPost.js
+++ src/components/blogPost.js
@@ -1,9 +1,55 @@
+import { Container } from 'semantic-ui-react';
+
+const BlogPost = ({post}) => <>
+  <Container text>
+    <h1>{post.title}</h1>
+    <p>{post && post.content}</p>
 
 
-const BlogPost = (post) => (
-  <>
-    <h1>I am blog post</h1>
-  </>
-)
+    {console.log('wats posty', post.content)}
+
+  </Container>
+</>
+
+  // <Header as='h2'>Craziest Post!</Header>
+  // <p>
+  //   Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+  //   ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+  //   magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+  //   ultricies nec, pellentesque eu, pretium quis, sem.
+  // </p>
+  // <p>
+  //   Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+  //   ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+  //   magnis dis parturient montes, nascetur ridiculus mus. 
+  // </p>
+
+  // <Divider hidden />
+  // <Divider hidden />
+
+  // <Container text>
+  // <Header as='h2'>Craziest Post!</Header>
+  // <p>
+  //   Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+  //   ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+  //   magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+  //   ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+  //   quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+  //   arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+  //   Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+  //   viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
+  //   Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+  // </p>
+  // <p>
+  //   Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+  //   ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+  //   magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+  //   ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+  //   quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+  //   arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+  //   Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+  //   dapibus.
+  // </p>
+  // </Container>
 
 export default BlogPost;
--- src/containers/BlogPage.js
+++ src/containers/BlogPage.js
@@ -1,6 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { Container, Header, Divider, Button, Modal, Icon, Form, Input, Select, Message } from 'semantic-ui-react';
+import { collection, query, onSnapshot, addDoc } from "firebase/firestore";
+import {db} from '../firebase';
+// import { addDoc, collection } from "firebase/firestore";
+
+
 import BlogPost from '../components/blogPost';
-import { Container, Header, Divider, Button, Modal, Icon } from 'semantic-ui-react';
 
 const samplePosts = [
   {
@@ -26,6 +31,12 @@ const samplePosts = [
   }
 ];
 
+const defaultFields = {
+  public: true,
+  title: '',
+  content: '',
+}
+
 // Takes in data from database
 // Uses component to render each post
 // Add button only shows if admin logged in flag
@@ -33,67 +44,72 @@ const samplePosts = [
 // Submit button updates database and then state
 
 const Blog = () => {
-  const [open, setOpen] = useState(false);
+  const [ allPosts, setAllPosts] = useState([]);
+  const [ sField, setSField ] = useState(defaultFields);
+  const [ error, setError ] = useState(false);
+  const [ open, setOpen ] = useState(false);
+
+  const updateSField = useCallback((key, value) => {
+    setSField(sField => {
+      return {
+        ...sField,
+        [key]: value,
+      };
+    });
+  }, []);
+  
+  const handleSubmit = async() => {
+    // You should really Validate the fields first
+    if (sField.pair.length > 0) {
+        await addDoc(collection(db, "blogposts"), {
+          ...sField,
+          created: new Date()
+      })
+      .then(() => console.log('successfully entered'))
+      .catch(err => console.error("error dun happen",err))
+      setError(false);
+    }
+    else {
+      setError(true);
+    }
+    
+    setOpen(false)
+    setSField(defaultFields);
+  }
+
+  useEffect(() => {
+    const q = query(collection(db, 'blogposts'))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setAllPosts(querySnapshot.docs.map(doc => {
+        let item = doc.data();
+        item.created = toDateTime(item.created.seconds);
+        return item
+      }))
+    })
+
+    return () => unsubscribe();
+  },[])
+
+  function toDateTime(secs) {
+    let t = new Date(1970, 0, 1); // Epoch
+    t.setSeconds(secs);
+    return t;
+  }
+
+  // console.log('all the posty', allPosts.length)
 
   return (
-    <>
-      <Header as='h1'>Blog Page</Header>
+    <div style={{ padding: "20px"}}>
+      <Header as='h1' style={{fontSize: '42px'}}>Blog Page</Header>
 
-      <Container text>
-        <Header as='h2'>Craziest Post!</Header>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-          ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-          ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-          quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-          arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-          Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-          viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-          Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-          ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-          ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-          quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-          arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-          Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
-          dapibus.
-        </p>
-      </Container>
-      
-      <Divider hidden />
-      <Divider hidden />
-
-      <Container text>
-        <Header as='h2'>Craziest Post!</Header>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-          ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-          ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-          quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-          arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-          Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-          viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-          Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-          ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-          ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-          quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-          arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-          Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
-          dapibus.
-        </p>
-      </Container>
+      {
+        allPosts.length > 0 ? allPosts.map((post) => {
+          return <BlogPost post={post} />
+        }) : "There are currently no posts."
+      }
 
       <Modal
-        basic
+        // basic
         onClose={() => setOpen(false)}
         onOpen={() => setOpen(true)}
         open={open}
@@ -102,14 +118,53 @@ const Blog = () => {
       >
         <Header icon>
           <Icon name='archive' />
-          Archive Old Messages
+          {/* Archive Old Messages */}
         </Header>
 
         <Modal.Content>
-          <p>
-            Your inbox is getting full, would you like us to enable automatic
-            archiving of old messages?
-          </p>
+            
+          <Form 
+            size="big"
+            onSubmit={handleSubmit}
+            error={error}
+          >
+          <Form.Group widths='equal'>
+            <Form.Field
+              control={Input}
+              name="pair"
+              label='Currency Pair:'
+              placeholder='BTC-USDT'
+              value={sField.pair}
+              onChange={(e,data) => updateSField("pair", data.value)}
+            />
+          </Form.Group>
+
+
+          <Form.Group widths="5">
+            <Form.Checkbox 
+              toggle 
+              label="Sup or Res?"
+              checked={sField.supres}
+              onClick={() => updateSField("supres",  !sField.supres)}
+            />
+            <Form.Checkbox 
+              toggle 
+              label="Instinct"
+              checked={sField.inst}
+              onClick={() => updateSField("inst",  !sField.inst)}
+            />
+            
+          </Form.Group>
+          {/* {error} */}
+          <Message
+            error
+            header='Error Occurred'
+            content="You forgot to fill out a field."
+          />
+          <Form.Field control={Button}>Add Trade</Form.Field>
+          </Form>
+
+
         </Modal.Content>
         
         <Modal.Actions>
@@ -123,7 +178,7 @@ const Blog = () => {
 
       </Modal>
 
-    </>
+    </div>
   )
 }
 
