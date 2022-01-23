9a4782e4daf52f547ccc95380828f633e6d65e37
barendan <barendan@gmail.com>
2022-01-23 17:54:47 -0500

    finish no auth page

--- src/containers/NoAuth.js
+++ src/containers/NoAuth.js
@@ -2,41 +2,43 @@ import React from 'react';
 
 const NoAuth = () => {
 
-  return <>
-    <div id="login-button">
-      <img src="https://dqcgrsy5v35b9.cloudfront.net/cruiseplanner/assets/img/icons/login-w-icon.png">
-      </img>
-    </div>
-    <div id="container">
-      <h1>Log In</h1>
-      <span className="close-btn">
-        <img src="https://cdn4.iconfinder.com/data/icons/miu/22/circle_close_delete_-128.png"></img>
-      </span>
+  return (
+    <div className="noauth-container">
+      {/* <div id="login-button">
+        <img src="https://dqcgrsy5v35b9.cloudfront.net/cruiseplanner/assets/img/icons/login-w-icon.png">
+        </img>
+      </div> */}
+      <div id="container">
+        <h1>Log In</h1>
+        <span className="close-btn">
+          <img src="https://cdn4.iconfinder.com/data/icons/miu/22/circle_close_delete_-128.png"></img>
+        </span>
 
-      <form>
-        <input type="email" name="email" placeholder="E-mail"/>
-        <input type="password" name="pass" placeholder="Password"/>
-        <a href="#">Log in</a>
-        <div id="remember-container">
-          <input type="checkbox" id="checkbox-2-1" class="checkbox" checked="checked"/>
-          <span id="remember">Remember me</span>
-          <span id="forgotten">Forgotten password</span>
-        </div>
-      </form>
-    </div>
+        <form>
+          <input type="email" name="email" placeholder="E-mail"/>
+          <input type="password" name="pass" placeholder="Password"/>
+          <a href="#">Log in</a>
+          <div id="remember-container">
+            <input type="checkbox" id="checkbox-2-1" class="checkbox" checked="checked"/>
+            <span id="remember">Remember me</span>
+            <span id="forgotten">Forgotten password</span>
+          </div>
+        </form>
+      </div>
 
-    <div id="forgotten-container">
-      <h1>Forgotten</h1>
-      <span class="close-btn">
-        <img src="https://cdn4.iconfinder.com/data/icons/miu/22/circle_close_delete_-128.png"></img>
-      </span>
+      <div id="forgotten-container">
+        <h1>Forgotten</h1>
+        <span class="close-btn">
+          <img src="https://cdn4.iconfinder.com/data/icons/miu/22/circle_close_delete_-128.png"></img>
+        </span>
 
-      <form>
-        <input type="email" name="email" placeholder="E-mail" />
-        <a href="#" class="orange-btn">Get new password</a>
-      </form>
+        <form>
+          <input type="email" name="email" placeholder="E-mail" />
+          <a href="#" class="orange-btn">Get new password</a>
+        </form>
+      </div>
     </div>
-  </>
+  )
 }
 
 export default NoAuth;
