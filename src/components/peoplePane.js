5f9df14987fefad5bf0b976fa991bfbdc4bfeb69
barendan <barendan@gmail.com>
2022-01-28 16:52:30 -0500

    make people pane dynamic and display data

--- src/components/peoplePane.js
+++ src/components/peoplePane.js
@@ -1,27 +1,105 @@
-import { Card, Icon } from 'semantic-ui-react';
+import {
+  Card,
+  Icon,
+  Button,
+  Form,
+  Input,
+  Select,
+  Message,
+  Radio,
+  TextArea,
+} from 'semantic-ui-react';
 
-const PeoplePane = () => {
-
-  const extra = (
-    <a>
-      <Icon name='user' />
-      16 Friends
-    </a>
-  )
+const PeoplePane = ({data}) => {
 
   return (
     <div className="peo-pane">
 
-
-
       <Card
-        image='/male2.png'
-        header='Elliot Baker'
-        meta='Friend'
-        // description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
-        // extra={extra}
+        image='/male1.png'
+        // header='Elliot Baker'
+        // meta='Friend'
       />
 
+      <Form size="big">
+        <Form.Group widths='equal'>
+          <Form.Field
+            control={Input}
+            label='First name'
+            placeholder='First name'
+            value={data.firstName}
+            readOnly
+          />
+          <Form.Field
+            control={Input}
+            label='Last name'
+            placeholder='Last name'
+            value={data.lastName}
+            readOnly
+          />
+          <Form.Field
+            control={Select}
+            label='Gender'
+            placeholder={data.gender}
+            // value={data.gender}
+            readOnly
+          />
+        </Form.Group>
+
+        <Form.Group widths='equal'>
+          <Form.Field
+            control={Input}
+            label='Location'
+            placeholder='Location'
+            value={data.location}
+          />
+          <Form.Field
+            control={Input}
+            label='Career'
+            placeholder='Career'
+            value={data.career}
+          />
+          <Form.Field
+            control={Select}
+            label='How we met'
+            // options={options.howmet}
+            placeholder={data.howmet}
+            // value={data.howmet}
+          />
+        </Form.Group>
+
+        <Form.Group inline>
+          <label>Status</label>
+          <Form.Field
+            control={Radio}
+            label='Single'
+            value='Single'
+            checked={data.status === 'Single'}
+          />
+          <Form.Field
+            control={Radio}
+            label='Taken'
+            value='Taken'
+            checked={data.status === 'Taken'}
+          />
+          <Form.Field
+            control={Radio}
+            label='Unknown'
+            value='Unknown'
+            checked={data.status === 'Unknown'}
+          />
+        </Form.Group>
+
+        <Form.Field
+          control={TextArea}
+          label='Additional Notes'
+          placeholder='What else is there...'
+          value={data.extra}
+          readOnly
+        />
+
+        {/* <Form.Field positive control={Button}>Submit</Form.Field> */}
+      </Form>
 
     </div>
   )
